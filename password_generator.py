54dd204d691aa8836c19090c96d912671e954b9c
TyriqOdle <tyriqodle2005@gmail.com>
2025-10-19 17:27:18 -0400

    Finished V1 of the app

---
+++ password_generator.py
@@ -0,0 +1,47 @@
+import random
+
+# Opens the password file in append mode.
+file = open("passwords.txt", "a")
+
+password = ""
+# Letters
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+# Numbers
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+# Special characters
+special_chars = [
+    '!', '@', '#', '$', '%', '^', '&', '*',
+]
+
+user_length = int(input("1. Short (10 Characters)" \
+"2. Medium (15 characters)" \
+"3 Long (24 Characters)" \
+"Select a length: "))
+
+# Determaining the length based on the input
+
+if user_length == 1:
+    length = 10
+elif user_length == 2:
+    length = 15
+else:
+    length = 24
+
+
+
+for i in range(length):
+    password += random.choice(random.choice(letters + numbers + special_chars))
+
+
+file.write(password + "\n")
+file.close()
+
+print(password)
+
